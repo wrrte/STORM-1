e63be93ab1267e546d808dc944cb44b31a0ae269
choemj <wrrtewdqsw@gmail.com>
2026-08-16 20:32:17 +0900

    feat: update training queue with new Atari environments and set starting seed to 2000

--- populate_queue.sh
+++ populate_queue.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 QUEUE_FILE="job_queue.txt"
-START_SEED=1
+START_SEED=2000
 
 # Original games list from the training scripts
 games=(
-    Gopher MsPacman Frostbite Hero ChopperCommand Jamesbond Asterix Qbert RoadRunner Seaquest UpNDown Alien Assault BankHeist BattleZone Boxing Breakout CrazyClimber DemonAttack Freeway Kangaroo Krull KungFuMaster Pong PrivateEye Amidar
+    Qbert
+Hero
+MsPacman
+Gopher
+Asterix
+UpNDown
+Jamesbond
+Seaquest
 )
 
 echo "Populating $QUEUE_FILE with default games..."
@@ -13,7 +20,6 @@ echo "Populating $QUEUE_FILE with default games..."
 for env_name in "${games[@]}"; do
     current_seed=${START_SEED}
     JOB="python -u train.py -n \"${env_name}-life_done-wm_2L512D8H-100k-seed${current_seed}\" -seed ${current_seed} -config_path \"config_files/STORM.yaml\" -env_name \"ALE/${env_name}-v5\" -trajectory_path \"D_TRAJ/${env_name}.pkl\" JointTrainAgent.Retrieval.enable False
-python -u train.py -n \"${env_name}-life_done-wm_2L512D8H-100k-seed${current_seed}\" -seed ${current_seed} -config_path \"config_files/STORM.yaml\" -env_name \"ALE/${env_name}-v5\" -trajectory_path \"D_TRAJ/${env_name}.pkl\" JointTrainAgent.Retrieval.enable True JointTrainAgent.Retrieval.warmup_steps 20000
     "
     
     echo "$JOB" >> "$QUEUE_FILE"
