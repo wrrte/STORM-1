0767ead7a70cf4377c9232039e5613ce1707ba72
choemj <wrrtewdqsw@gmail.com>
2026-08-20 15:25:42 +0900

    5090 job queue

--- populate_queue.sh
+++ populate_queue.sh
@@ -5,18 +5,14 @@ START_SEED=10
 
 # Original games list from the training scripts
 games=(
-    Frostbite Qbert BattleZone UpNDown
+    Alien Amidar Assault BankHeist BattleZone Boxing DemonAttack 
 )
 
 echo "Populating $QUEUE_FILE with default games..."
 
 for env_name in "${games[@]}"; do
     current_seed1=${START_SEED}
-    current_seed1=${START_SEED+700}
-    current_seed1=${START_SEED+3700}
     JOB="python -u train.py -n \"${env_name}-life_done-wm_2L512D8H-100k-seed${current_seed1}\" -seed ${current_seed1} -config_path \"config_files/STORM.yaml\" -env_name \"ALE/${env_name}-v5\" -trajectory_path \"D_TRAJ/${env_name}.pkl\" JointTrainAgent.Retrieval.enable True
-python -u train.py -n \"${env_name}-life_done-wm_2L512D8H-100k-seed${current_seed2}\" -seed ${current_seed2} -config_path \"config_files/STORM.yaml\" -env_name \"ALE/${env_name}-v5\" -trajectory_path \"D_TRAJ/${env_name}.pkl\" JointTrainAgent.Retrieval.enable True
-python -u train.py -n \"${env_name}-life_done-wm_2L512D8H-100k-seed${current_seed3}\" -seed ${current_seed3} -config_path \"config_files/STORM.yaml\" -env_name \"ALE/${env_name}-v5\" -trajectory_path \"D_TRAJ/${env_name}.pkl\" JointTrainAgent.Retrieval.enable True
     "
     
     echo "$JOB" >> "$QUEUE_FILE"
